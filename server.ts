6417987a58819e2ce5a1d092c46fc1d30ea8f44b
DLionAppsPersonal <63153805+DLionApps@users.noreply.github.com>
2020-10-30 22:24:46 +0530

    minor change for heroku deploy test

--- server.ts
+++ server.ts
@@ -36,7 +36,7 @@ require("./src/controllers/ServiceContoller")(app);
 require("./src/controllers/VehicleController")(app);
 
 const server = app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+  console.log(`API listening on port ${port}`);
 });
 
 module.exports = server;
