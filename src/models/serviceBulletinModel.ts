2901440f49942ebb716e1e753e2e32927659cc33
DLionAppsPersonal <63153805+DLionApps@users.noreply.github.com>
2021-01-17 10:12:50 +0530

    removed comments

--- src/models/serviceBulletinModel.ts
+++ src/models/serviceBulletinModel.ts
@@ -1,19 +1,19 @@
-// import * as mongoose from "mongoose";
-// import * as uuid from "uuid";
+import * as mongoose from "mongoose";
+import * as uuid from "uuid";
 
-// const ServiceBulletinSchema = new mongoose.Schema({
-//   _id: String,
-//   title: String,
-//   applicableVehicleTypes: [Number],
-//   isDeleted: { type: Boolean, default: false },
-// });
+const ServiceBulletinSchema = new mongoose.Schema({
+  _id: String,
+  title: String,
+  applicableVehicleTypes: [Number],
+  isDeleted: { type: Boolean, default: false },
+});
 
-// ServiceBulletinSchema.pre("save", function (next) {
-//   const doc = this;
-//   if (doc.isNew) {
-//     doc._id = uuid.v1();
-//   }
-//   next();
-// });
+ServiceBulletinSchema.pre("save", function (next) {
+  const doc = this;
+  if (doc.isNew) {
+    doc._id = uuid.v1();
+  }
+  next();
+});
 
-// export default mongoose.model("serviceBulletin", ServiceBulletinSchema);
+export default mongoose.model("serviceBulletin", ServiceBulletinSchema);
